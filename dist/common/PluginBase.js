36950631687c439beb77b3f295f1268fcf9dea16
trujunzhang <trujunzhang@gmail.com>
2018-01-31 21:03:27 +0800

    with image-sequence.
    https://github.com/madsleejensen/react-native-image-sequence

--- dist/common/PluginBase.js
+++ dist/common/PluginBase.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.ImageSequence = exports.SwipeRow = exports.Root = exports.Segment = exports.TabHeading = exports.ActionSheet = exports.Toast = exports.View = exports.Text = exports.Grid = exports.Row = exports.Col = exports.Textarea = exports.Label = exports.Item = exports.DeckSwiper = exports.Separator = exports.ListItem = exports.List = exports.Picker = exports.FooterTab = exports.Tabs = exports.Tab = exports.Footer = exports.Content = exports.Container = exports.Switch = exports.Spinner = exports.H3 = exports.H2 = exports.H1 = exports.CardItem = exports.Card = exports.Thumbnail = exports.Radio = exports.CheckBox = exports.Badge = exports.Body = exports.Right = exports.Left = exports.Fab = exports.Title = exports.Input = exports.InputGroup = exports.Form = exports.Header = exports.IonIcon = exports.Icon = exports.Button = exports.Drawer = exports.StyleProvider = undefined;
+exports.LinearGradient = exports.ImageSequence = exports.SwipeRow = exports.Root = exports.Segment = exports.TabHeading = exports.ActionSheet = exports.Toast = exports.View = exports.Text = exports.Grid = exports.Row = exports.Col = exports.Textarea = exports.Label = exports.Item = exports.DeckSwiper = exports.Separator = exports.ListItem = exports.List = exports.Picker = exports.FooterTab = exports.Tabs = exports.Tab = exports.Footer = exports.Content = exports.Container = exports.Switch = exports.Spinner = exports.H3 = exports.H2 = exports.H1 = exports.CardItem = exports.Card = exports.Thumbnail = exports.Radio = exports.CheckBox = exports.Badge = exports.Body = exports.Right = exports.Left = exports.Fab = exports.Title = exports.Input = exports.InputGroup = exports.Form = exports.Header = exports.IonIcon = exports.Icon = exports.Button = exports.Drawer = exports.StyleProvider = undefined;
 
 var _nativeBase = require('native-base');
 
@@ -12,6 +12,10 @@ var _reactNativeImageSequence = require('react-native-image-sequence');
 var _reactNativeImageSequence2 = _interopRequireDefault(_reactNativeImageSequence);
 
 var _Ionicons = require('react-native-vector-icons/Ionicons');
+
+var _reactNativeLinearGradient = require('react-native-linear-gradient');
+
+var _reactNativeLinearGradient2 = _interopRequireDefault(_reactNativeLinearGradient);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -66,3 +70,4 @@ exports.Segment = _nativeBase.Segment;
 exports.Root = _nativeBase.Root;
 exports.SwipeRow = _nativeBase.SwipeRow;
 exports.ImageSequence = _reactNativeImageSequence2.default;
+exports.LinearGradient = _reactNativeLinearGradient2.default;
